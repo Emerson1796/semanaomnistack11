7be3535db1f632713841fc37cb0e4a36c1dc2558
Emerson1796 <emerson.gomes.t@gmail.com>
2020-03-30 23:03:41 -0300

    inclusão de teste e segurança

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,16 +1,19 @@
+const connection = require("../database/connection")
+const generateUniqueId = require('../utils/generateUniqueId');
 const crypto = require('crypto');
-const connection = require('../database/connection');
 
 module.exports = {
-    async index (request, response){
+
+    async index (request, response) {
         const ongs = await connection('ongs').select('*');
+    
         return response.json(ongs);
     },
 
-    async create(request, response){
-        const {name, email, whatsapp, city, uf} = request.body;
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
@@ -19,8 +22,8 @@ module.exports = {
             whatsapp,
             city,
             uf,
-        })
+        });
 
         return response.json({ id });
     }
-};
+}
